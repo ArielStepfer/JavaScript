9915c812946dd5675443cd35d0b1d72484f15f00
Ariel <arielstepfer93@gmail.com>
2024-11-04 12:36:22 -0300

    Commit definitivo

--- PreEntrega1/Preentrega1.js
+++ PreEntrega1/Preentrega1.js
@@ -1,32 +1,26 @@
-// Función para determinar cuántos y qué tipo de animales pueden entrar en la casa
 function calcularCantidadAnimales() {
-    // Solicitar cantidad de ambientes
     const cantidadAmbientes = parseInt(prompt("Ingrese la cantidad de ambientes en el domicilio:"));
     
-    // Validar que la cantidad de ambientes sea un número válido
+    
     if (isNaN(cantidadAmbientes) || cantidadAmbientes <= 0) {
         console.log("La cantidad de ambientes ingresada no es válida.");
         return;
     }
+        let metrosCuadradosTotales = 0;
 
-    // Solicitar metros cuadrados de cada ambiente
-    let metrosCuadradosTotales = 0;
-    for (let i = 1; i <= cantidadAmbientes; i++) {
-        const metrosCuadrados = parseFloat(prompt(`Ingresd los metros cuadrados del ambiente ${i}:`));
+        for (let i = 1; i <= cantidadAmbientes; i++) {
+        const metrosCuadrados = parseFloat(prompt(`Ingrese los metros cuadrados del ambiente ${i}:`));
         
-        // Validar que los metros cuadrados sean válidos
-        if (isNaN(metrosCuadrados) || metrosCuadrados <= 0) {
+            if (isNaN(metrosCuadrados) || metrosCuadrados <= 0) {
             console.log("Los metros cuadrados ingresados no son válidos.");
             return;
         }
-        
         metrosCuadradosTotales += metrosCuadrados;
     }
 
-    // Preguntar si la casa tiene espacios techados y si posee espacio verde
-       const tieneEspacioVerde = confirm("¿La casa tiene espacio verde?");
+        const tieneEspacioVerde = confirm("¿La casa tiene espacio verde?");
 
-    // Condiciones para albergar animales
+    // Condicional para ver si cumple la condicion para albergar
     if (cantidadAmbientes >= 3 && metrosCuadradosTotales > 60 && tieneEspacioVerde) {
         console.log("La casa puede albergar un perro grande.");
     } else if (cantidadAmbientes <= 3 && metrosCuadradosTotales <= 60 || !tieneEspacioVerde) {
@@ -36,5 +30,4 @@ function calcularCantidadAnimales() {
     }
 }
 
-// Ejecutar el programa
 calcularCantidadAnimales();
